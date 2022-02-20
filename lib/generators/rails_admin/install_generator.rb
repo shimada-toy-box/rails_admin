c87f1cecb6df1a20b8a1c2d5f2814c165b3d30fc
M.Shibuya <mit.shibuya@gmail.com>
2022-02-20 19:31:21 +0900

    Add Importmap support
    Closes #3399

--- lib/generators/rails_admin/install_generator.rb
+++ lib/generators/rails_admin/install_generator.rb
@@ -27,10 +27,14 @@ module RailsAdmin
       case asset
       when 'webpack'
         configure_for_webpack
+      when 'importmap'
+        configure_for_importmap
       when 'webpacker'
         configure_for_webpacker5
       when 'sprockets'
         configure_for_sprockets
+      else
+        raise "Unknown asset source: #{asset}"
       end
     end
 
@@ -41,6 +45,8 @@ module RailsAdmin
 
       if Rails.root.join('webpack.config.js').exist?
         'webpack'
+      elsif Rails.root.join('config/importmap.rb').exist?
+        'importmap'
       elsif defined?(Webpacker)
         'webpacker'
       else
@@ -54,17 +60,56 @@ module RailsAdmin
 
     def configure_for_webpacker5
       run "yarn add rails_admin@#{RailsAdmin::Version.js}"
-      @scss_relative_dir = '../stylesheets/'
-      template 'rails_admin.js.erb', 'app/javascript/packs/rails_admin.js'
-      template 'rails_admin.scss', 'app/javascript/stylesheets/rails_admin.scss'
+      template 'rails_admin.webpacker.js', 'app/javascript/packs/rails_admin.js'
+      template 'rails_admin.scss.erb', 'app/javascript/stylesheets/rails_admin.scss'
     end
 
     def configure_for_webpack
       run "yarn add rails_admin@#{RailsAdmin::Version.js} css-loader mini-css-extract-plugin sass sass-loader"
-      @scss_relative_dir = './'
-      template 'rails_admin.js.erb', 'app/javascript/rails_admin.js'
-      template 'rails_admin.scss', 'app/javascript/rails_admin.scss'
+      template 'rails_admin.js', 'app/javascript/rails_admin.js'
+      template 'rails_admin.scss.erb', 'app/javascript/rails_admin.scss'
       template 'webpack.config.js', 'webpack.config.js'
+    end
+
+    def configure_for_importmap
+      run "yarn add rails_admin@#{RailsAdmin::Version.js}"
+      template 'rails_admin.js', 'app/javascript/rails_admin.js'
+      require_relative 'importmap_formatter'
+      add_file 'config/importmap.rails_admin.rb', ImportmapFormatter.new.format
+      setup_css
+    end
+
+    def setup_css
+      gem 'cssbundling-rails'
+      rake 'css:install:sass'
+
+      @fa_font_path = '.'
+      template 'rails_admin.scss.erb', 'app/assets/stylesheets/rails_admin.scss'
+      asset_config = %{Rails.application.config.assets.paths << Rails.root.join("node_modules/@fortawesome/fontawesome-free/webfonts")\n}
+      if File.exist? File.join(destination_root, 'config/initializers/assets.rb')
+        append_to_file 'config/initializers/assets.rb', asset_config
+      else
+        add_file 'config/initializers/assets.rb', asset_config
+      end
+
+      display 'Add build:css script'
+      build_script = 'sass ./app/assets/stylesheets/rails_admin.scss:./app/assets/builds/rails_admin.css --no-source-map --load-path=node_modules'
+      package = begin
+        JSON.parse(File.read(File.join(destination_root, 'package.json')))
+      rescue Errno::ENOENT, JSON::ParserError
+        {}
+      end
+      if package['scripts'] && package['scripts']['build:css']
+        say <<-MESSAGE.gsub(/^ {10}/, ''), :red
+          Append or merge into existing "scripts": { "build:css": "#{build_script}" } to your package.json.
+          For example, if you're already have application.sass.css for the sass build, the resulting script would look like:
+            sass ./app/assets/stylesheets/application.sass.scss:./app/assets/builds/application.css ./app/assets/stylesheets/rails_admin.scss:./app/assets/builds/rails_admin.css --no-source-map --load-path=node_modules
+        MESSAGE
+      else
+        package['scripts'] ||= {}
+        package['scripts']['build:css'] = build_script
+        add_file 'package.json', JSON.pretty_generate(package)
+      end
     end
   end
 end
